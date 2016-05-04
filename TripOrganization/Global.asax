7d3fc4d7d9c62173adf1036de4fab809c6768c02
Kacper Werema <kacper.werema@gmail.com>
2016-05-04 22:09:30 +0200

    Start

---
+++ TripOrganization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TripOrganization.MvcApplication" Language="C#" %>
---
+++ TripOrganization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripOrganization.Startup))]
+namespace TripOrganization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
